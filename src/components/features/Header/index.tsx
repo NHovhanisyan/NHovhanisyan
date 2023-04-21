954e093430bb8e43d1ddc3de02f8635a609306c6
Narek Hovhannisyan <n.hovo@bk.ru>
2023-04-21 21:45:12 +0400

    updated header

--- src/components/features/Header/index.tsx
+++ src/components/features/Header/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router'
 import MenuIcon from '@mui/icons-material/Menu'
 import { useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
+import { Typography } from '@material-ui/core'
 import { Box, IconButton } from '@mui/material'
+import KeyboardDoubleArrowRightRoundedIcon from '@mui/icons-material/KeyboardDoubleArrowRightRounded'
 import logo from '../../../assets/Logo/NarTech.png'
+import { Button } from '../../shared/Button'
 import { NavLinks } from '../../../utils/Navbar/constants'
 import { NavBarLink } from '../../shared/NavLink'
 import DrawerComponent from '../../shared/Drawer'
@@ -13,6 +17,7 @@ import styles from './styles.module.scss'
 export const Header = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
+  const navigate = useNavigate()
   const theme = useTheme()
   const isMediumScreen = useMediaQuery(theme.breakpoints.down('md'))
 
@@ -23,10 +28,22 @@ export const Header = () => {
       <Box className={styles.content}>
         {!isMediumScreen ? (
           <>
-            <img className={styles.logo} src={logo} alt="logo" />
+            <img
+              className={styles.logo}
+              src={logo}
+              alt="logo"
+              onClick={() => navigate('/')}
+            />
             {NavLinks.map((link) => (
               <NavBarLink key={link.path} {...link} />
             ))}
+            <Button
+              style={{ marginTop: 0, padding: '8px 25px' }}
+              onClick={() => navigate('contact-me')}
+            >
+              <Typography>Hire Me&nbsp;</Typography>
+              <KeyboardDoubleArrowRightRoundedIcon />
+            </Button>
           </>
         ) : (
           <IconButton onClick={() => setIsDrawerOpen(true)}>
@@ -42,11 +59,7 @@ export const Header = () => {
         variant="persistent"
       >
         {NavLinks.map((link) => (
-          <NavBarLink
-            key={link.path}
-            {...link}
-            onClick={handleClose}
-          />
+          <NavBarLink key={link.path} {...link} onClick={handleClose} />
         ))}
       </DrawerComponent>
     </Box>
