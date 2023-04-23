188a37c807d3840926e8bb8fb3f514f0e21bc017
Narek Hovhannisyan <n.hovo@bk.ru>
2023-04-23 15:07:36 +0400

    created special skills

--- src/components/features/SpecialSkills/index.tsx
+++ src/components/features/SpecialSkills/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Box } from '@mui/material'
-import { ServiceCard } from '../../components/features/Service_Card/index'
-import { Services } from '../../utils/My_Services/constants'
+import { ServiceCard } from '../Service_Card/index'
+import { Services } from '../../../utils/My_Services/constants'
 
 import styles from './styles.module.scss'
-import { SectionHeader } from '../../components/shared/SectionHeader'
-import { SpecialSkillCard } from '../../components/features/SpecialSkillCard'
-import { Skills } from '../../utils/Skills/constants'
-import CVButton from '../../components/features/CvButton'
+import { SectionHeader } from '../../shared/SectionHeader'
+import { SpecialSkillCard } from '../SpecialSkillCard'
+import { Skills } from '../../../utils/Skills/constants'
+import CVButton from '../CvButton'
 
 export const SpecialSkills = () => {
   return (
@@ -17,7 +17,6 @@ export const SpecialSkills = () => {
           title={'My Special Skill Field Here'}
           label={'Special Skills'}
         />
-        <CVButton />
       </Box>
       <Box className={styles.skills}>
         {Skills.map((specialSkill) => (
---
+++ src/utils/Statistics/constants.ts
@@ -0,0 +1,21 @@
+import Person4OutlinedIcon from '@mui/icons-material/Person4Outlined'
+import DisplaySettingsOutlinedIcon from '@mui/icons-material/DisplaySettingsOutlined'
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined'
+
+export const StatisticsInfo = [
+  {
+    title: 'Happy Clients',
+    count: 50,
+    icon: Person4OutlinedIcon,
+  },
+  {
+    title: 'Projects Completed',
+    count: 80,
+    icon: DisplaySettingsOutlinedIcon,
+  },
+  {
+    title: 'Years Of Experience',
+    count: 4,
+    icon: CalendarMonthOutlinedIcon,
+  },
+]
