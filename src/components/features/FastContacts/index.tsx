86eb857caac5f84f61b8bf831faf3c31f216ef9d
Narek Hovhannisyan <n.hovo@bk.ru>
2023-04-23 15:02:14 +0400

    created fast contacts

---
+++ src/components/features/FastContacts/index.tsx
@@ -0,0 +1,40 @@
+import { Box, Typography } from '@mui/material'
+import CallOutlinedIcon from '@mui/icons-material/CallOutlined'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+
+import styles from './styles.module.scss'
+import { Button } from '../../shared/Button'
+
+const FastContacts = () => {
+  return (
+    <Box className={styles.fastContacts}>
+      <Box className={styles.contactContainer}>
+        <Button className="fastContactBtn">
+          <CallOutlinedIcon sx={{ color: 'white' }} />
+        </Button>
+        <Typography className={styles.contact}>+37477810019</Typography>
+      </Box>
+      <Box className={styles.contactContainer}>
+        <Button className="fastContactBtn">
+          <EmailOutlinedIcon sx={{ color: 'white' }} />
+        </Button>
+        <Typography className={styles.contact}>n.hovo2002@gmail.com</Typography>
+      </Box>
+      <Box className={styles.contactContainer}>
+        <Button className="fastContactBtn">
+          <FacebookOutlinedIcon sx={{ color: 'white' }} />
+        </Button>
+        <Button className="fastContactBtn">
+          <CallOutlinedIcon sx={{ color: 'white' }} />
+        </Button>
+        <Button className="fastContactBtn">
+          <LinkedInIcon sx={{ color: 'white' }} />
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default FastContacts
